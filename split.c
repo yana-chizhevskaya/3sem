ba25033f1f3285f572c6e5b4757ffc865afc8bbf
Yana <yana@yana-VirtualBox.(none)>
2015-09-15 22:56:56 +0400

    first ex

--- split.c
+++ split.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-	
-void Split(char* string, char* delimiters, char*** tokens, int* tokensCount){
-		char* p = NULL;
-		p = strtok(string,delimiters);	
-		while (p) {
-      if (p) printf("%s\n", p);
-			p = strtok(NULL, delimiters);
-  }
+#include <stdlib.h>
 
+void Split(char* string, char* delimiters, char*** tokens, int* tokensCount)
+{
+	char* p = NULL;
+	int k;
+	p = strtok(string,delimiters);
+	if (p)
+		k = 1;
+	else
+		k = 0;	
+	while (p)
+		{
+    	*(*tokens+k) = p;
+		p = strtok(NULL, delimiters);
+		k++;
+ 		}
+	*tokensCount = k;
 }
 
-
-
-	int main() {
-	
+int main() {
+	int i = 0, n;
 	char string[200] = "", delimiters[100] = "";
-	char*** words;
-	int n = 0;
+	char** words = (char**)malloc(sizeof(char*)*100);
 	gets(string);
 	gets(delimiters);
-	Split (string, delimiters, words, &n);
-
-return 0;
+	Split (string, delimiters, &words, &n);
+	for (i = 1; i < n; i++)
+		printf ("%s\n", words[i]);
+	printf("\n");
+	return 0;
 }
 
 
