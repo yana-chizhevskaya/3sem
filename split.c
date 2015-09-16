cb01f11363ff192a9950917b70770ddec1985d8c
Yana <yana@yana-VirtualBox.(none)>
2015-09-16 19:19:34 +0400

    fixed

--- split.c
+++ split.c
@@ -5,31 +5,11 @@
 void Split(char* string, char* delimiters, char*** tokens, int* tokensCount)
 {
 	char* p = NULL;
-<<<<<<< HEAD
 	int k = 0;
 	p = strtok(string,delimiters);	
 	while (p)
 		{
     	(*tokens)[k] = p;
-=======
-	int k;
-	p = strtok(string,delimiters);
-  /*
-   * Непонятно, что вы этим хотели сказать.
-   * Если вдруг без этого не обойтись, то можно было бы воспользоваться тернарным оператором
-   * k = p ? 1 : 0;
-   */
-	if (p)
-		k = 1;
-	else
-		k = 0;	
-	while (p)
-		{
-      /*
-       * по-моему запись (*tokens)[k] более понятна, чем *(*tokens+k)
-       */
-    	*(*tokens+k) = p;
->>>>>>> c1eac4aca433efda22a5c09709c03b444c1a2b93
 		p = strtok(NULL, delimiters);
 		k++;
  		}
@@ -37,7 +17,6 @@ void Split(char* string, char* delimiters, char*** tokens, int* tokensCount)
 }
 
 int main() {
-<<<<<<< HEAD
 	int i = 0, tokensCount;
 	char string[200] = "", delimiters[100] = "";
 	char** words = (char**)malloc(sizeof(char*) * 100);
@@ -47,34 +26,6 @@ int main() {
 	for (i = 0; i < tokensCount; i++)
 		printf ("%s\n", words[i]);
 	free(words);
-=======
-	int i = 0, n;
-  /*
-   * FIXIT: Не заводите в коде магических чисел, о смысле которых надо докадываться. Объявите константу MaxStringSize и MaxDelimetersCount соотвественно.
-   * + старайтесь называть переменные более осмысленно: почему бы вместо n не написать tokensCount, ведь за ней ровно это кроетя.
-   */
-	char string[200] = "", delimiters[100] = "";
-  /*
-   * FIXIT:
-   * ставьте отступы вокруг бинарных операторов
-   */
-	char** words = (char**)malloc(sizeof(char*)*100);
-	gets(string);
-	gets(delimiters);
-	Split (string, delimiters, &words, &n);
-  /*
-   * FIXIT: почему цикл от 1, а не от 0?
-   * вы где-то лишний раз увеличили n на единицу. 
-   * ф-я split должна вернуть число слов, а не число слов плюс 1.
-   */
-	for (i = 1; i < n; i++)
-		printf ("%s\n", words[i]);
-  
-  /*
-   * надо освободить память
-   */
-	printf("\n");
->>>>>>> c1eac4aca433efda22a5c09709c03b444c1a2b93
 	return 0;
 }
 
