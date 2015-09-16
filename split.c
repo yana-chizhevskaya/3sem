dd7793a25e81f1db74944c9e1b4e7327f0d08551
Yana <yana@yana-VirtualBox.(none)>
2015-09-16 14:09:45 +0400

    last version

--- split.c
+++ split.c
@@ -5,15 +5,11 @@
 void Split(char* string, char* delimiters, char*** tokens, int* tokensCount)
 {
 	char* p = NULL;
-	int k;
-	p = strtok(string,delimiters);
-	if (p)
-		k = 1;
-	else
-		k = 0;	
+	int k = 0;
+	p = strtok(string,delimiters);	
 	while (p)
 		{
-    	*(*tokens+k) = p;
+    	(*tokens)[k] = p;
 		p = strtok(NULL, delimiters);
 		k++;
  		}
@@ -21,16 +17,15 @@ void Split(char* string, char* delimiters, char*** tokens, int* tokensCount)
 }
 
 int main() {
-	int i = 0, n;
+	int i = 0, tokensCount;
 	char string[200] = "", delimiters[100] = "";
-	char** words = (char**)malloc(sizeof(char*)*100);
+	char** words = (char**)malloc(sizeof(char*) * 100);
 	gets(string);
 	gets(delimiters);
-	Split (string, delimiters, &words, &n);
-	for (i = 1; i < n; i++)
+	Split (string, delimiters, &words, &tokensCount);
+	for (i = 0; i < tokensCount; i++)
 		printf ("%s\n", words[i]);
-	printf("\n");
+	free(words);
 	return 0;
 }
-
 
