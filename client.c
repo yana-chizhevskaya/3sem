8bd2c4d6ed4a4c70ca2d7cee54854a5b3f453047
yana-chizhevskaya <yana_chizhevskaya@mail.ru>
2015-11-24 17:05:02 +0300

    chat

---
+++ client.c
@@ -0,0 +1,95 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+  int sockfd;
+  int n, len;
+  char sendline[1000], recvline[1000], name[1000], message[1000], buf[] = ":    ";
+  struct sockaddr_in servaddr, cliaddr;
+  
+  if (argc != 2)
+  {
+    printf("Usage: ./a.out <IP address>\n");
+    exit(1);
+  }
+  if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+  {
+    perror(NULL);
+    exit(1);
+  }
+  
+  bzero(&cliaddr, sizeof(cliaddr));
+  cliaddr.sin_family = AF_INET;
+  cliaddr.sin_port = htons(0);
+  cliaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  
+  if (bind(sockfd, (struct sockaddr*)&cliaddr, sizeof(cliaddr)) < 0)
+  {
+    perror(NULL);
+    close(sockfd);
+    exit(1);
+  }
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(62000);
+  if (inet_aton(argv[1], &servaddr.sin_addr) == 0)
+  {
+    printf("Invalid IP address\n");
+    close(sockfd);
+    exit(1);
+  }
+  printf("Welcome to the chat. Enter your name\n");
+  gets(name);
+
+	
+  if (sendto(sockfd, sendline, strlen(sendline) + 1, 0,
+    (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+	  {
+		perror(NULL);
+		close(sockfd);
+		exit(1);
+	  }
+  pid_t pid = fork();
+  if(pid == 0)
+  {
+  
+	  while(1) {	
+		  strcpy(sendline, name);
+		  fgets(message, 1000, stdin);
+		  strcat(sendline, buf);
+		  strcat(sendline, message);
+		  if (sendto(sockfd, sendline, strlen(sendline) + 1, 0,
+			(struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+			  {
+				perror(NULL);
+				close(sockfd);
+				exit(1);
+			  }
+		  bzero(&sendline, strlen(sendline));
+	  }
+  }
+  else
+  {
+  	while(1)
+  	{
+	  if ((n = recvfrom(sockfd, recvline, 1000, 0, (struct sockaddr*) NULL, NULL)) < 0)
+	  {
+		perror(NULL);
+		close(sockfd);
+		exit(1);
+	  }
+	  printf("%s", recvline);
+	}
+  }
+  close(sockfd);
+ 
+  return 0;
+}
---
+++ server.c
@@ -0,0 +1,104 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+  int sockfd;
+  int clilen, n, i = 0;
+  char line[1000];
+  char buf[100];
+  struct sockaddr_in servaddr, cliaddr;
+  
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(62000);
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  
+  if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+  {
+    perror(NULL);
+    exit(1);
+  } else
+  {
+    printf("Sock fd:%d\n", sockfd);
+  }
+  
+  if (bind(sockfd, (struct sockaddr*) &servaddr, 
+    sizeof(servaddr)) < 0)
+  {
+    perror(NULL);
+    close(sockfd);
+    exit(1);
+  }
+  
+  struct User
+  {
+    int port;
+    int ip;
+	char name[100];
+  }; 
+  
+  struct User users[100];
+  int userscount = 0;
+  clilen = sizeof(cliaddr);
+  while (1)
+  {
+	bzero(line, 1000);
+    if ((n = recvfrom(sockfd, line, 999, 0, (struct sockaddr*)&cliaddr, &clilen)) < 0)
+    {
+      perror(NULL);
+      close(sockfd);
+      exit(1);
+    }
+    printf("%s", line);
+  
+    
+		int newUser = -1;
+		
+		for(i = 0; i < userscount; i++)
+		{
+		  if((cliaddr.sin_port == users[i].port) && (cliaddr.sin_addr.s_addr 			  == users[i].ip))
+		  newUser = i;
+		}
+		
+		if(newUser < 0)
+		{
+		  users[userscount].port = cliaddr.sin_port; 
+		  users[userscount].ip = cliaddr.sin_addr.s_addr;
+		  printf("someone's connected, ip %d, port, %d\n", users[userscount].ip, users[userscount].port);
+		  userscount++;
+		}
+		else
+		{
+		  for(i = 0; i < userscount; i++)
+		  {
+			if(i != newUser)
+			{
+				printf("sending message %s to ip %d, port %d", line, users[i].ip, users[i].port);
+				bzero(&cliaddr, sizeof(cliaddr));
+  				cliaddr.sin_family = AF_INET;
+			  	cliaddr.sin_port = users[i].port;
+			  	cliaddr.sin_addr.s_addr = users[i].ip;
+
+		 		if (sendto(sockfd, line, strlen(line)+1, 0, (struct 			 
+								sockaddr*)&cliaddr, clilen) < 0)
+		   		{
+				  perror(NULL);
+				  close(sockfd);
+				  exit(1);
+		  		 }
+			}
+		  }
+		}
+	
+	}  
+	  
+  return 0;
+}
